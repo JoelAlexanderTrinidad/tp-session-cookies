030e262243b19f1afd159b90aaf026c44ff38820
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-08-28 16:43:08 -0300

    micro desafío 1 hecho

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -10,16 +10,19 @@ const controller = {
     indexPost: (req, res) => {
         const errors = validationResult(req);
         // return res.send(req.body)
+
         if(errors.isEmpty()){
-            let nuevoUsuario = {
+            /* let nuevoUsuario = {
                 nombre: req.body.nombre,
                 color: req.body.color,
                 email: req.body.email,
                 edad: req.body.edad
             }
             usuarios.push(nuevoUsuario);
-            fs.writeFileSync(path.resolve(__dirname, '..', 'data', 'usersDatabase.json'),JSON.stringify(usuarios, null, 3), 'utf-8');
-            return res.send('Usuario registrado');
+            fs.writeFileSync(path.resolve(__dirname, '..', 'data', 'usersDatabase.json'),JSON.stringify(usuarios, null, 3), 'utf-8'); */
+            return res.render('index',{
+                old: req.body
+            })
         }else{
             return res.render('index',{
                 errors: errors.mapped(),
