abd81fb269eb96cb46edb39339509abccbed96f4
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-08-29 18:52:09 -0300

    tp session cookies

--- src/middlewares/cookieCheck.js
+++ src/middlewares/cookieCheck.js
@@ -2,6 +2,5 @@ module.exports = (req,res,next) =>{
     if(req.cookies.usuario){
         req.session.userLogin = req.cookies.usuario
     }
-    console.log('>>>>>>>>>>>>>>>>>>>',req.session.userLogin)
     next()
 }
